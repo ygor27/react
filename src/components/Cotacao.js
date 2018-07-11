cbb2420bb7c163e4e78710a16e94c8b7a270e566
Ygor <ygor.silva@kroton.com.br>
2018-07-11 18:25:02 -0300

    add components

---
+++ src/components/Cotacao.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Cotacao extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Cotação de Seguros</h2>
+      </div>
+    );
+  }
+}
+ 
+export default Cotacao;
